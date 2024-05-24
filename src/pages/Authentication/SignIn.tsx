87d23a1832e908b93c4c8f41b8eeed7c47c5a529
unknown <sedighe.ghanbary@gmail.com>
2024-05-24 22:28:33 +0400

    Add project

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -37,6 +37,7 @@ const SignIn = () => {
   };
 
   function handle_login(e: any) {
+     console.log(1200);
      
     e.preventDefault();
 
@@ -63,6 +64,7 @@ console.log(500,isInvalid);
 
   return (
     <>
+    {console.log(200)}
       <div className="flex h-screen items-center justify-center rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="flex flex-wrap items-center">
           <div className="hidden w-full xl:block xl:w-1/2">
